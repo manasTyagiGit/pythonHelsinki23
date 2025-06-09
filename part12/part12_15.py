d1c49a396ee947e44ae3dc413e17bab08ab30a9f
manasTyagiGit <manastyagi.ind@gmail.com>
2025-06-09 23:37:55 +0530

    Add additional part12 files

---
+++ part12/part12_15.py
@@ -0,0 +1,22 @@
+def price_under_4_euros(product):
+    return product[1] < 4
+
+
+def search (products: list, criterion = callable) :
+    ret_list = []
+    for p in products :
+        if criterion(p) :
+            ret_list.append(p)
+
+    return ret_list
+
+
+products = [("banana", 5.95, 12), ("apple", 3.95, 3), ("orange", 4.50, 2), ("watermelon", 4.95, 22), ("kale", 0.99, 1)]
+
+for product in search(products, price_under_4_euros):
+    print(product)
+
+
+for product in search(products, lambda t: t[2]>10):                 # for quantity greater thean 10
+    print(product)
+
